107bd4981bd9114d1c39185b6ffaf10a4ab41246
Tim Roberts <ten3roberts@gmail.com>
2020-12-22 20:39:11 +0100

    Add volume notification

--- volctrl.sh
+++ volctrl.sh
@@ -1,8 +1,15 @@
+#/bin/bash
 case $1 in
     toggle) pamixer -t ;;
     mute) pamixer -m ;;
     up) pamixer -i 2 ;;
     down) pamixer -d 2 ;;
 esac
+
+# Arbitrary but unique message id
+msgId="991049"
+
+volume=`pamixer --get-volume-human`
+dunstify -a "volume" -u low -i audio-volume-high -r "$msgId" "Volume:  $volume"
 
 pkill -RTMIN+10 dwmblocks
