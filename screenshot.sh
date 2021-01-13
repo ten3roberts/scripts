7a52ecf40adbfe000ac23476fada3d0e785517cd
Tim Roberts <ten3roberts@gmail.com>
2021-01-13 12:44:18 +0100

    Minor tweaks

--- screenshot.sh
+++ screenshot.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 SCROTCMD="scrot $HOME/Pictures/screenshots/%F_%H-%M-%S_scrot.png"
 case "$1" in
-    region) sleep 0.2 && $SCROTCMD --line style=dash -sf -e 'xclip -selection c -t image/png < $f' ;;
-    screen) $SCROTCMD -m ;;
+    region) sleep 0.2 && $SCROTCMD --line style=dash -sf -e 'xclip -selection c -t image/png < $f' && notify-send "Captured region" ;;
+    screen) $SCROTCMD -m && notify-send "Captured screen" ;;
     *) echo "Unknown option $1" ;;
 esac
 
